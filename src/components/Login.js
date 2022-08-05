31332b1f403e06ccb0a44612a9a194427975710f
NguyenDuyLong2021 <19130128@st.hcmuaf.edu.vn>
2022-08-05 22:46:39 +0700

    update code contact

--- src/components/Login.js
+++ src/components/Login.js
@@ -38,6 +38,7 @@ export default function Login({ navigation }) {
         saveData({
           providerData: user.providerData,
           stsTokenManager: user.stsTokenManager,
+          idUser: user.uid
         });
 
         // console.log(userCredential)
